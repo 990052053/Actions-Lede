d9c7e2e76c5e765884cc6cfe83030fee0ca4d64f
kakashi <77133451+990052053@users.noreply.github.com>
2022-05-22 05:50:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,26 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Add Custom feeds source
-echo 'src-git Lienol https://github.com/Lienol/openwrt-package.git' >>feeds.conf.default
-echo 'src-git ssrplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-
-# Add passwall && helloworld
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/lean/passwall
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/vssr
-
-# Add & Replace theme source
-rm -rf package/lean/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
 # Add kenzok8 feeds source
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 
